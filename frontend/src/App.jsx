0da2564fe723966f89efdebe6fa621cc6538771d
haseeb491 <uhaseeb@348@gmail.com>
2024-08-25 19:47:41 +0500

    8-fetching posts from the database(forYou + following)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -21,7 +21,7 @@ function App() {
 				const data = await res.json();
 				if(data.error) return null
 				if(!res.ok) throw new Error(data.error || "Something went wrong");
-				console.log("authUser is here:",data);
+				// console.log("authUser is here:",data);
 				return data;
 			} catch (error) {
 				throw new Error(error)

89219ffc7c90f75dbca7c308373e96871645f038
haseeb491 <uhaseeb@348@gmail.com>
2024-08-26 23:15:45 +0500

    13-like and unlike post

--- frontend/src/components/common/Post.jsx
+++ frontend/src/components/common/Post.jsx
@@ -13,7 +13,7 @@ const Post = ({ post }) => {
 	const [comment, setComment] = useState("");
 	const {data : authUser} = useQuery({queryKey : ["authUser"]})
 	const queryClient = useQueryClient();
-	const {mutate : deletePost,isPending} = useMutation({
+	const {mutate : deletePost,isPending :isDeleting} = useMutation({
 		mutationFn : async()=>{
 			try {
 				const res = await fetch(`/api/posts/${post._id}`,{
@@ -32,8 +32,41 @@ const Post = ({ post }) => {
 			queryClient.invalidateQueries({queryKey : ['posts']})
 		}
 	})
+	const {mutate: likePost , isPending :isLiking} = useMutation({
+		mutationFn : async() =>{
+			try {
+				const res = await fetch(`/api/posts/like/${post._id}`,{
+					method : "POST",
+				});
+
+				const data = await res.json();
+				if(!res.ok) throw new Error(data.error || "Something went wrong" );
+				return data;
+			} catch (error) {
+				throw new Error(error.message);
+			}
+		},
+		onSuccess : (updatedLikes) =>{
+			toast.success("liked post successfully");
+			//not the best approach for UI/UX , bc it will refetch all the posts
+			// queryClient.invalidateQueries({queryKey : ['posts']});
+			//better solution : update the cache directly for the post
+			queryClient.setQueryData(['posts'], (oldData)=>{
+				return oldData.map(p =>{
+					if(p._id === post._id) {
+						return {...p,likes : updatedLikes}
+					}
+					return p
+				})
+			})
+
+		},
+		onError : ()=>{
+			toast.error(error.message)
+		}
+	})
 	const postOwner = post.user;
-	const isLiked = false;
+	const isLiked = post.likes.includes(authUser._id);
 
 
 	const isMyPost = authUser._id === post.user._id;
@@ -50,7 +83,10 @@ const Post = ({ post }) => {
 		e.preventDefault();
 	};
 
-	const handleLikePost = () => {};
+	const handleLikePost = () => {
+		if(isLiking) return;
+		likePost();
+	};
 
 	return (
 		<>
@@ -72,9 +108,9 @@ const Post = ({ post }) => {
 						</span>
 						{isMyPost && (
 							<span className='flex justify-end flex-1'>
-								{!isPending && (<FaTrash className='cursor-pointer hover:text-red-500'
+								{!isDeleting && (<FaTrash className='cursor-pointer hover:text-red-500'
 								onClick={handleDeletePost} />)}
-								{isPending && (
+								{isDeleting && (
 									<LoadingSpinner size="sm" />
 								)}
 							</span>
@@ -94,7 +130,8 @@ const Post = ({ post }) => {
 						<div className='flex gap-4 items-center w-2/3 justify-between'>
 							<div
 								className='flex gap-1 items-center cursor-pointer group'
-								onClick={() => document.getElementById("comments_modal" + post._id).showModal()}
+								onClick={() => document.getElementById
+								("comments_modal" + post._id).showModal()}
 							>
 								<FaRegComment className='w-4 h-4  text-slate-500 group-hover:text-sky-400' />
 								<span className='text-sm text-slate-500 group-hover:text-sky-400'>
@@ -144,7 +181,7 @@ const Post = ({ post }) => {
 										/>
 										<button className='btn btn-primary rounded-full btn-sm text-white px-4'>
 											{isCommenting ? (
-												<span className='loading loading-spinner loading-md'></span>
+												<LoadingSpinner size="md" />
 											) : (
 												"Post"
 											)}
@@ -160,10 +197,11 @@ const Post = ({ post }) => {
 								<span className='text-sm text-slate-500 group-hover:text-green-500'>0</span>
 							</div>
 							<div className='flex gap-1 items-center group cursor-pointer' onClick={handleLikePost}>
-								{!isLiked && (
+								{isLiking && <LoadingSpinner size="sm" />}
+								{!isLiked && !isLiking && (
 									<FaRegHeart className='w-4 h-4 cursor-pointer text-slate-500 group-hover:text-pink-500' />
 								)}
-								{isLiked && <FaRegHeart className='w-4 h-4 cursor-pointer text-pink-500 ' />}
+								{isLiked && !isLiking && <FaRegHeart className='w-4 h-4 cursor-pointer text-pink-500 ' />}
 
 								<span
 									className={`text-sm text-slate-500 group-hover:text-pink-500 ${
